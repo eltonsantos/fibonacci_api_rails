f1672c0791b9d1408e68a44f2dd03b80665a52f9
Elton Santos <eltonaxl@hotmail.com>
2016-12-16 02:10:01 -0200

    Created a class Fibonacci with a method for generate fibonacci and used in the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+
+  # Function that call the model method calculate_fibonacci
+  def fibonacci
+    @fibonacci = Fibonacci.calculate_fibonacci(params[:quantity])
+  end
+
 end
