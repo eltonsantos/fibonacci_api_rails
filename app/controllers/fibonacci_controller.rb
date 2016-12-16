eeed1baa2d139d757435ed16c922a6436c768394
Elton Santos <eltonaxl@hotmail.com>
2016-12-16 15:59:54 -0200

    Finsish fibonacci API Application, enter a number and return the sequence

--- app/controllers/fibonacci_controller.rb
+++ app/controllers/fibonacci_controller.rb
@@ -1,6 +1,5 @@
 class FibonacciController < ApplicationController
 
-  # GET /fibonacci
   # Function that call the model method calculate_fibonacci
   def calculate
     @fibonacci = Fibonacci.sequence_fibonacci(params[:quantity].to_i)
